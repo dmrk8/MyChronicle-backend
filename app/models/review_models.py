c310c2663767ef7495625a831d3c4f9fbdfc87a8
AnilDemirok <anildemirok8@gmail.com>
2025-11-13 22:17:24 +0300

    refactored the model, added response model for uniform data representation

--- app/models/review_models.py
+++ app/models/review_models.py
@@ -1,24 +1,33 @@
 
 from datetime import datetime
-from typing import Optional
-from pydantic import BaseModel
+from typing import Any, Optional
+from pydantic import BaseModel, Field
 
 class ReviewCreate(BaseModel):
-    media_id: int
+    media_id: int = Field(..., serialization_alias="mediaId")
     title: str
     type : str
-    review: str
-    rating: float
+    review: Optional[str]
+    rating: Optional[float]
+    is_favorite: bool = Field(..., serialization_alias="isFavorite")
 
 class ReviewDB(ReviewCreate):
     id : Optional[str] = None
-    user_id : str
-    created_at : datetime
-    updated_at : datetime
+    user_id : str = Field(..., serialization_alias="userId")
+    created_at : datetime = Field(serialization_alias="createdAt")
+    updated_at : datetime = Field(serialization_alias="updatedAt")
 
 class ReviewUpdate(BaseModel):
     id : str
     review: Optional[str] = None
     rating: Optional[float] = None
 
-
+class ReviewResponse(BaseModel):
+    message: str
+    review_id: Optional[str] = None
+    matched_count: Optional[int] = None
+    modified_count: Optional[int] = None
+    deleted_count: Optional[int] = None
+    updated_at: Optional[Any] = None
+    data: Optional[Any] = None  # For returning review data or lists
+    acknowledged: Optional[bool] = None
